ccc922939245988066fb30ea53b42294d9e8ab6e
JoseSaez07 <146@alumnos.fpsumma.es>
2025-10-29 11:38:05 +0100

    s

---
+++ Notaporletra1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Notaporletra1 {
+public String nota(String notraLetra){
+return switch (notraLetra.toUpperCase()){
+case "A" -> "Notasobresaliente";
+case "B" -> "NotasobreNotable";
+case "C"->  "NotasobreBien";
+case "D"->  "NotasobreSuficiente";
+case "F"->  "NotasobreInsuficiente";
+default -> "Invalido";
+
+
+};
+
+}
+
+public static void main(String[] args) {
+Scanner sc new Scanner(System.in);
+System.out.println("Dime tu nota");
+String valor = sc.nextLine();
+String resultadoSwich= semaforo.accionSemaforoSwitch(color);
+System.out.println(resultado);
+System.out.println(resultadoSwich);
+sc.close();
+sc.close();
+
+}
+}
