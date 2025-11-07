7d342645d8d0c4fdcbf2a15ea3f08ec7666b8e34
JoseSaez07 <146@alumnos.fpsumma.es>
2025-11-07 13:14:06 +0100

    a
    Clase

--- MenuInteractivo.java
+++ MenuInteractivo.java
@@ -1,32 +1,37 @@
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuInteractivo {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-int valor;
-do{
-    System.out.println("---Menú---");
-    System.out.println("1.Hola");
-    System.out.println("2.Hora");
-    System.out.println("3.Salir");
-     valor = sc.nextInt();
-    switch (valor) {
-     case 1  -> System.out.println("Hola " + valor);  
-     case 2  -> {String hora = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));  
-    System.out.println(hora); } 
-     case 3  -> System.out.println("Adiós");
-     default ->System.out.println("Opcion no valida"); 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int valor = 0;
+        do {
+            System.out.println("---Menú---");
+            System.out.println("1.Hola");
+            System.out.println("2.Hora");
+            System.out.println("3.Salir");
+
+            try {
+                valor = sc.nextInt();
+                sc.nextLine();
+                switch (valor) {
+                    case 1 -> System.out.println("Hola " + valor);
+                    case 2 -> {
+                        String hora = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+                        System.out.println(hora);
+                    }
+                    case 3 -> System.out.println("Adiós");
+                    default -> System.out.println("Opción no valida");
+                }
+
+            } catch (InputMismatchException e) {
+
+                System.out.println("Opción invalida");
+            }
+
+        } while (valor != 3);
+        sc.close();
     }
-    break;
-}while(valor ==3 );
-    
-     
 }
-}
-
-    
-
-
-
