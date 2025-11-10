3ca859070681c4d9fc59eaef5ce488ef9bbccc38
JoseSaez07 <146@alumnos.fpsumma.es>
2025-11-10 15:46:10 +0100

    s
    s

--- AdivinaElNumero.java
+++ AdivinaElNumero.java
@@ -1,17 +1,42 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class AdivinaElNumero {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-final  int MAX = 100;
-final  int Min = 0;
-    public static void secret() {
-        int tries = 0;
+
+    public static void main(String[] args) {
+        final int MIN = 0;
+        final int MAX = 100;
+        int secreto = MIN + new Random().nextInt(MAX - MIN + 1);
+        int intentos = 0;
+        Scanner sc = new Scanner(System.in);
+        int numero = 0;
+        boolean acertado = false;
+
+        System.out.println("Estoy pensando un número del " + MIN + " al " + MAX + ".");
+
+        while (!acertado) {
+            System.out.print("Tu intento: ");
+
+            // Comprobamos si el valor introducido es un número entero
+            if (!sc.hasNextInt()) {
+                System.out.println("❌ Error: por favor, introduce un número entero.");
+                sc.next(); // limpiar entrada incorrecta
+                continue;
+            }
+
+            numero = sc.nextInt();
+            intentos++;
+
+            if (numero < secreto) {
+                System.out.println("Mayor");
+            } else if (numero > secreto) {
+                System.out.println("Menor");
+            } else {
+                System.out.println("¡Acertaste en " + intentos + " intentos!");
+                acertado = true;
+            }
+        }
+
+        sc.close();
     }
-
-    }
-
-
 }
-
-
--- ParOimpar.java
+++ ParOimpar.java
@@ -1,15 +1,30 @@
 import java.util.Scanner;
 
 public class ParOimpar {
-public static String main(String[] args){
-Scanner sc = new  Scanner(System.in);
-System.out.println("Dime un numero");
-int numero = sc.nextInt();
+    public String parOimparOmultiplode(int valor) {
+        boolean esPar = (valor % 2 == 0);
+        boolean esMultiplode3 = (valor % 3 == 0);
+        if (esPar && esMultiplode3) {
+            return "Es par y es multiplo de 3";
+        } else if (esPar) {
+            return "Es par";
+        } else if (esMultiplode3) {
+            return "Es multiplo de 3";
+        } else if (!esPar && esMultiplode3) {
+            return "Es impar y multiplo de 3";
+        } else {
+            return "Es impar";
 
-boolean esPar = (numero % 2 == 0);
-boolean esMultiplode3 = (numero % 3 == 0);
-sc.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        ParOimpar programa = new ParOimpar();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Dime un numero: ");
+        int numero = sc.nextInt();
+        String resultado = programa.parOimparOmultiplode(numero);
+        System.out.println(resultado);
+        sc.close();
+    }
 }
-}
-
-
