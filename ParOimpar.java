ad62a3f452d6b1493cb3b1a7aea8617283c4b247
JoseSaez07 <146@alumnos.fpsumma.es>
2025-11-11 10:21:57 +0100

    ds
    s

--- ParOimpar.java
+++ ParOimpar.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
 
 public class ParOimpar {
-public static String main(String[] args){
-Scanner sc = new  Scanner(System.in);
-System.out.println("Dime un numero");
-int numero = sc.nextInt();
+    public String numeroA(int numero) {
+        boolean esPar = (numero % 2 == 0);
+        boolean esMultiplode3 = (numero % 3 == 0);
+        if (esPar && esMultiplode3) {
+            return "Es par y multiplo de 3";
+        } else if (!esPar && esMultiplode3) {
+            return "Es impar y multipo de 3";
+        } else if (esPar) {
+            return "Es par";
+        } else {
+            return "Es impar";
+        }
+    }
 
-boolean esPar = (numero % 2 == 0);
-boolean esMultiplode3 = (numero % 3 == 0);
-sc.close();
+    public static void main(String[] args) {
+        ParOimpar prueba = new ParOimpar();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Dime un numero: ");
+        int numero = sc.nextInt();
+        String resultado = prueba.numeroA(numero);
+        System.out.println(resultado);
+        sc.close();
+
+    }
 }
-}
-
-
